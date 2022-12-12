88c3a26ca5c434d54ec9926a4f3622050c128e82
Paul Maier <pauljustus279@gmail.com>
2022-12-12 22:30:05 +0100

    Fixed built

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/birklehof-teams-name-generator/",
-  plugins: [svelte(), ViteFaviconsPlugin("./src/assets/favicon.png")],
+  plugins: [svelte()],
 });
