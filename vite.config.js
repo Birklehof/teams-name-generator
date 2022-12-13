56385bee491dbdf95cb50d4fc693e45d7335a4ea
Paul Maier <pauljustus279@gmail.com>
2022-12-13 13:59:00 +0100

    Changed config after transfer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/birklehof-teams-name-generator/",
+  base: "/teams-name-generator/",
   plugins: [svelte()],
 });
